49758789404448e105ff5baacbe0d7cf5b752075
Aleksandr Petushkov <aleks.petushkov@gmail.com>
2016-04-08 18:09:18 +0600

    begin

---
+++ drob/drob.h
@@ -0,0 +1,21 @@
+#include <iostream>
+
+class drob
+{
+public:
+	
+protected:
+
+	unsigned int chis; //chislitel
+	unsigned int znam; //znamenatel
+	double epselon;
+
+
+
+
+
+
+
+	
+
+};
---
+++ drob/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+
+int ker()
+{
+	return 1;
+
+}
+
+int main()
+{
+	double error = 1.0e-10;
+	error *= 10000000000;
+	std::cout << (int)error;
+	std::cout << "";
+	ker();
+
+
+
+
+
+}
